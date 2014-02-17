b7f37f819c48502ef15e987085afc1b04791430b
Dave Huffman <daveh303@yahoo.com>
2014-02-17 18:12:40 -0500

    added comments and refactoring.

--- js/extensions.js
+++ js/extensions.js
@@ -1,10 +1,21 @@
+/**
+ * @overview Extensions to existing JavaScript object.
+ * @author Dave Huffman
+ * @copyright 2014
+ */
+
+// Handle environments that don't provide a console.
 if (typeof console === 'undefined') {
   console = {
+    debug: function () {}
+    dir: function () {},
     log: function () {},
-    dir: function () {}
   };
 }
 
+/**
+ * @returns {String} A capitalized version of this string.
+ */
 String.prototype.capitalize = function () {
   return this.charAt(0).toUpperCase() + this.slice(1);
 };
--- js/flash.js
+++ js/flash.js
@@ -1,149 +1,149 @@
+/** Lookup object for vocab key values. */
 var VALUES = {
-  /*kanji: { text: 'Kanji', tag: 'h1' },*/
   kana: { text: 'Kana', tag: 'h1' },
   romaji: { text: 'Rōmaji', tag: 'p' },
   english: { text: 'English', tag: 'p' }
 };
 
+/** Flash app namespace. */
 var Flash = new function () {
   var self = this,
       deck = null;
-  
+
+  /**
+   * A deck of flash cards.
+   * @constructor
+   * @params {Object} options - Configuration options.
+   */
   function Deck(options) {
     var self = this,
         vocabList = _.shuffle(options.vocab),
         cardIndex = 0,
         cardAnswer = false;
-        
+
+    /** Display the next card in the deck. */
     this.nextCard = function () {
-      console.log('asdfasdfasdfdsa ' + cardIndex);
+      console.log('nextCard() - card index ' + cardIndex);
       var card = null,
           questions = null,
           answers = null;
-            
+
       if (cardAnswer) {
-        // Move to the next card.
-        
-        console.log('next card, next');
-        
+        // Display the next card.
+        console.debug('next card, next');
+
         cardIndex++;
-        updateProgress(cardIndex + 1);
+        updateProgress(cardIndex);
         if (cardIndex === vocabList.length) {
           done();
           return;
         }
-        
-        /*questions = $('#card_div .card .question');
-        answers = $('#card_div .card .answer');
-        answers.css('display', 'none');
-        
-        card = vocabList[cardIndex];
-        for (key in card) {
-          questions.children('.card_text.' + key).text(card[key]);
-          answers.children('.card_text.' + key).text(card[key]);
-        }
-        
-        $('#card_div .card .question')
-            .css('display', 'normal')
-            .fadeIn();
-        cardAnswer = false;*/
-        
         renderCard(cardIndex);
       } else {
         // Display the answer.
-        
-        console.log('next card, answer');
-        
+        console.debug('next card, answer');
+
         $('#card_div .card .answer').fadeIn();
         cardAnswer = true;
       }
     }
-    
+
+    /**
+     * Draw a card on the screen.
+     * @param {Number} index - The index of the displayed card in the deck.
+     */
     function renderCard(index) {
       var card = vocabList[index],
           questions = $('#card_div .card .question'),
           answers = $('#card_div .card .answer');
-          
-      questions.hide();//.css('display', 'none');
-      answers.hide();//.css('display', 'none');
-      
+
+      questions.hide();
+      answers.hide();
+
       for (key in card) {
         questions.children('.card_text.' + key).text(card[key]);
         answers.children('.card_text.' + key).text(card[key]);
       }
-      
-      $('#card_div .card .question')
-          //.css('display', 'normal')
-          .fadeIn();
+
+      $('#card_div .card .question').fadeIn();
       cardAnswer = false;
     }
-    
-    function updateProgress(cardNumber) {
-      $('#deck_progress .bar').css('width', 
-          Math.round((cardNumber / options.vocab.length) * 100) + '%');
+
+    /**
+     * Update the UI progress indicator to match the player's current position
+     * in the deck.
+     * @param {Number} cardIndex - The current card's index.
+     */
+    function updateProgress(cardIndex) {
+      $('#deck_progress .bar').css('width',
+          Math.round(((cardIndex + 1) / options.vocab.length) * 100) + '%');
     }
-    
+
+    /** End the current study session. */
     function done() {
-      console.log('done');
+      console.debug('done');
       self.begin();
     }
-        
+
+    /** Start the study session. */
     this.begin = function () {
-      console.log('deck begin');
+      console.debug('deck begin');
       var card = $('#card_div .card'),
           question_span = null;
           selector = '',
           type = '',
           tag = '',
           i = 0;
-          
+
       cardIndex = 0;
       cardAnswer = false;
-      
-      card.find('.card_text').hide();//css('display', 'none');  
+
+      card.find('.card_text').hide();
       for (i = 0; i < options.questions.length; i++) {
         type = options.questions[i];
         tag = VALUES[type].tag
+        // TODO implement helper method for creating HTML for card text.
         $('.question').append('<' + tag + ' class="card_text ' + type + '">&nbsp;</' + tag + '/>');
-        // selector = '.question .card_text.' + options.questions[i].toLowerCase();
-        // console.log('[' + selector + '] found ' + card.find(selector));
-        // card.find(selector).css('display', 'normal');
       }
       for (i = 0; i < options.answers.length; i++) {
         type = options.answers[i];
         tag = VALUES[options.answers[i]].tag
         $('.answer').append('<' + tag + ' class="card_text ' + type + '">&nbsp;</' + tag + '/>');
-        // selector = '.answer .card_text.' + options.answers[i].toLowerCase();
-        // console.log('[' + selector + '] found ' + card.find(selector));
-        // card.find(selector).css('display', 'normal');
       }
       $('#card_div .card').unbind('click').click(self.nextCard);
-      
-      
-      $('#setup_div').hide();//css('display', 'none');  
-      $('.answer').hide();//css('display', 'none');  
+
+
+      $('#setup_div').hide();
+      $('.answer').hide();
       renderCard(0);
       $('#card_div').fadeIn();
-      //$('#deck_progress .bar').css('width', '0%');
-      updateProgress(1);
+      updateProgress(0);
       $('#deck_progress').fadeIn();
-      
     };
   }
-  
+
+  /**
+   * Query the DOM to retrieve the user's selection for a button set.
+   * @param {String} selector - jQuery selector for the button set.
+   * @returns {Array} the user's selection(s).
+   */
   function getButtonSelections(selector) {
     return $(selector).children('button.active').map(
         function () { return this.value; });
   }
-  
+
+  /**
+   * Query the DOM to get the user's selected options for a study session.
+   * @returns {Object} The user's selections.
+   */
   this.getOptions = function () {
     var cardSet = $('#input_cart_set').val();
-        options = { 
+        options = {
           questions: getButtonSelections('#form_setup #input_question'),
           answers: getButtonSelections('#form_setup #input_answer'),
           vocab: VOCAB_LIST
         };
-        
+
     // Validate the options.
     if (options.questions.size() === 0) {
       alert('You must choose at least one question option.');
@@ -153,76 +153,79 @@ var Flash = new function () {
       alert('You must choose at least one answer option.');
       return null;
     }
-    
+
     // Limit the vocab list to a set of cards matching the selected tag.
     if (typeof cardSet !== 'undefined' && cardSet !== 'all') {
-      options.vocab = _.select(VOCAB_LIST, function (v) { 
-        return v.tags.split(/,\s*/).indexOf(cardSet) >= 0; 
+      options.vocab = _.select(VOCAB_LIST, function (v) {
+        return v.tags.split(/,\s*/).indexOf(cardSet) >= 0;
       });
     }
-    
+
     return options;
   };
-  
+
+  /** Begin a study session. */
   this.begin = function () {
-    console.log('flash begin');
+    console.debug('flash begin');
     var options = self.getOptions();
     if (options !== null) {
       deck = new Deck(options);
       deck.begin();
     }
   };
-  
+
+  /** Return the user to the main menu. */
   this.reset = function () {
     deck = null;
     $('#card_div').hide();
     $('#deck_progress').fadeOut();
     $('#setup_div').fadeIn();
   };
-  
-  this.fillButtonGroup = function (groupSelector, data, onclick) {
+
+  /**
+   * Utility method; fill a button group with a set of options and behavior.
+   * @param groupSelector - jQuery selector for the button group.
+   * @param options - Object with data about options for the group.
+   * @param onclick - Optional event handler funtion for click events.
+   */
+  this.fillButtonGroup = function (groupSelector, options, onclick) {
     var group = $(groupSelector),
         key = '';
-    
-    for (key in data) {
+
+    for (key in options) {
       group.append('<button type="button" class="btn" value="' + key + '">'
-          + data[key].text + '</button>'); 
+          + options[key].text + '</button>');
     }
     if (typeof onclick !== 'undefined') {
       group.find('button').click(onclick);
     }
     group.button();
   };
-  
+
+  /** Fill the cart set UI select control with options. */
   this.fillCardSet = function () {
     var select = $('select#input_cart_set'),
         tags = _.uniq(_.flatten(_.map(VOCAB_LIST, function (w) { return w.tags.split(/,\s*/); } ))).sort();
-    
+
     _.each(tags, function (t) {
       select.append('<option value="' + t + '">' + t.capitalize() + '</option>');
     });
   };
-  
+
+  /** @returns The current flash card deck. */
   this.getDeck = function () {
     return deck;
   };
 }();
 
+// Start the app!
 $(function () {
-  // Setup Form.
-  // $('#form_setup #input_question').button();
-  // $('#form_setup #input_answer').button();
-  Flash.fillButtonGroup('#form_setup #input_question', VALUES);
-  Flash.fillButtonGroup('#form_setup #input_answer', VALUES);
-  Flash.fillCardSet();
-	//$('#deck_size_slider').slider();
-  
+  // Setup the header.
   $('#reset_button').click(Flash.reset);
   $('#start_button').click(Flash.begin);
-  // $('#autoplay_button').button().bind('click', function () {
   $('body').on('click', '#autoplay_button', function () {
     if ($(this).hasClass('active')) {
-      this.__timeout = setInterval(function () { 
+      this.__timeout = setInterval(function () {
         try {
           Flash.getDeck().nextCard();
         } catch (e) {}
@@ -231,4 +234,9 @@ $(function () {
       clearInterval(this.__timeout);
     }
   });
+
+  // Setup the form.
+  Flash.fillButtonGroup('#form_setup #input_question', VALUES);
+  Flash.fillButtonGroup('#form_setup #input_answer', VALUES);
+  Flash.fillCardSet();
 });
